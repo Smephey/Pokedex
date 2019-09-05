44b9008b7687bf9337cd3b68eac8239e9eb6fd63
Smephey <smephey@gmail.com>
2019-09-05 18:56:18 +0100

    Set up project structure

---
+++ src/app/modules/pokedex/pokedex.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonPageComponent } from './pages/pokemon-page/pokemon-page.component';
+
+
+
+@NgModule({
+  declarations: [PokemonPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokedexModule { }
