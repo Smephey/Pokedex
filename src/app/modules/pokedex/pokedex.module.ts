7acb197e24e79dde65e3354a1d776691de5b3cde
smephey <smephey@gmail.com>
2019-09-06 08:26:45 +0100

    Extracted navigation into separate component
    So that it can be used across all components without duplicating code

--- src/app/modules/pokedex/pokedex.module.ts
+++ src/app/modules/pokedex/pokedex.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common';
 import {PokemonPageComponent} from './pages/pokemon-page/pokemon-page.component';
 import {HomePageComponent} from './pages/home-page/home-page.component';
 import {AppRoutingModule} from '../../app-routing.module';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
 
 
 @NgModule({
-  declarations: [PokemonPageComponent, HomePageComponent],
+  declarations: [PokemonPageComponent, HomePageComponent, MainNavigationComponent],
   exports: [
     HomePageComponent
   ],
