2e80cd6759f9efe96f70d57fe5602f8d0351fd35
Michal <smephey@gmail.com>
2019-10-04 13:58:12 +0100

    Added subscriptions

--- src/app/modules/pokedex/pages/lazy-loaded-pokemon/lazy-loaded-pokemon.component.ts
+++ src/app/modules/pokedex/pages/lazy-loaded-pokemon/lazy-loaded-pokemon.component.ts
@@ -1,13 +1,14 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
 import {PokemonService} from '../../services/pokemon.service';
 import {IPokemonCard} from '../../types/IPokemon';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'lazy-loaded-pokemon-page',
   templateUrl: './lazy-loaded-pokemon.component.html',
   styleUrls: ['./lazy-loaded-pokemon.component.scss']
 })
-export class LazyLoadedPokemonComponent implements OnInit {
+export class LazyLoadedPokemonComponent implements OnInit, OnDestroy {
 
   allPokemon: Array<IPokemonCard>;
 
@@ -17,6 +18,7 @@ export class LazyLoadedPokemonComponent implements OnInit {
   private _pokemonService: PokemonService;
   private _offset = 0;
   private _limit = 50;
+  private _subscriptions: Subscription = new Subscription();
 
   constructor(pokemonService: PokemonService) {
     this._pokemonService = pokemonService;
@@ -25,11 +27,14 @@ export class LazyLoadedPokemonComponent implements OnInit {
   ngOnInit(): void {
     const offset = this._offset;
     const limit = this._limit;
-    this._pokemonService.getPokemon(offset, limit)
+    this._subscriptions.add(this._pokemonService.getPokemon(offset, limit)
       .subscribe((pokemonList) => {
         this.allPokemon = pokemonList;
-      });
+      }));
+  }
 
+  ngOnDestroy(): void {
+    this._subscriptions.unsubscribe();
   }
 
   goToDetails($event: number) {
@@ -39,11 +44,11 @@ export class LazyLoadedPokemonComponent implements OnInit {
   loadMorePokemon() {
     this._offset += 50;
     this._limit = 50;
-    this._pokemonService.getPokemon(this._offset, this._limit)
+    this._subscriptions.add(this._pokemonService.getPokemon(this._offset, this._limit)
       .subscribe((pokemonList) => {
         pokemonList.forEach((pokemon) => {
           this.allPokemon.push(pokemon);
         });
-      });
+      }));
   }
 }
